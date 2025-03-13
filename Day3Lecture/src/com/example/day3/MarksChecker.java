5a28670d18dd6a9467671d6298bdb7312848bb67
minmaung132h <minmaung132h@prestigein.com>
2025-03-13 12:57:07 +0700

    added Ex4

---
+++ Day3Lecture/src/com/example/day3/MarksChecker.java
@@ -0,0 +1,18 @@
+package com.example.day3;
+
+public class MarksChecker {
+
+	public static void main(String[] args) {
+		
+		int subject1;
+		subject1 = 39;
+		
+		
+		int subject2 = 20;
+		
+		boolean result = subject1 >= 40 || subject2 >= 40;
+		System.out.println("Exam Result : "+ result);
+
+	}
+
+}
---
+++ Day3Lecture/src/com/example/day3/TestOperator.java
@@ -0,0 +1,14 @@
+package com.example.day3;
+
+public class TestOperator {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		System.out.println(a--); // a = a+1
+		System.out.println(a);
+		
+
+	}
+
+}
